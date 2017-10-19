ae8be98cbd1305735147a09e374c5f9449f40e59
dzhmud <zhmud.dmitriy@gmail.com>
2017-10-19 18:10:54 +0300

    Problem26

---
+++ src/main/java/me/dzhmud/euler/Problem27.java
@@ -0,0 +1,19 @@
+package me.dzhmud.euler;
+
+/**
+ * TODO comment this class
+ *
+ * @author dzhmud
+ */
+public class Problem27 implements EulerSolution {
+
+	public static void main(String[] args) {
+		System.out.println(new Problem27().getAnswer());
+	}
+
+	@Override
+	public String getAnswer() {
+		return null;
+	}
+
+}
--- src/test/java/me/dzhmud/euler/AnswerCorrectnessTest.java
+++ src/test/java/me/dzhmud/euler/AnswerCorrectnessTest.java
@@ -19,31 +19,32 @@ public class AnswerCorrectnessTest {
 
 	@Test
 	public void testAnswers() {
-		test(Problem01.class, "233168");
-		test(Problem02.class, "4613732");
-		test(Problem03.class, "6857");
-		test(Problem04.class, "906609");
-		test(Problem05.class, "232792560");
-		test(Problem06.class, "25164150");
-		test(Problem07.class, "104743");
-		test(Problem08.class, "23514624000");
-		test(Problem09.class, "31875000");
-		test(Problem10.class, "142913828922");
-		test(Problem11.class, "70600674");
-		test(Problem12.class, "76576500");
-		test(Problem13.class, "5537376230");
-		test(Problem14.class, "837799");
-		test(Problem15.class, "137846528820");
-		test(Problem16.class, "1366");
-		test(Problem17.class, "21124");
-		test(Problem18.class, "1074");
-		test(Problem19.class, "171");
-		test(Problem20.class, "648");
-		test(Problem21.class, "31626");
-		test(Problem22.class, "871198282");
-		test(Problem23.class, "4179871");
-		test(Problem24.class, "2783915460");
-		test(Problem25.class, "4782");
+//		test(Problem01.class, "233168");
+//		test(Problem02.class, "4613732");
+//		test(Problem03.class, "6857");
+//		test(Problem04.class, "906609");
+//		test(Problem05.class, "232792560");
+//		test(Problem06.class, "25164150");
+//		test(Problem07.class, "104743");
+//		test(Problem08.class, "23514624000");
+//		test(Problem09.class, "31875000");
+//		test(Problem10.class, "142913828922");
+//		test(Problem11.class, "70600674");
+//		test(Problem12.class, "76576500");
+//		test(Problem13.class, "5537376230");
+//		test(Problem14.class, "837799");
+//		test(Problem15.class, "137846528820");
+//		test(Problem16.class, "1366");
+//		test(Problem17.class, "21124");
+//		test(Problem18.class, "1074");
+//		test(Problem19.class, "171");
+//		test(Problem20.class, "648");
+//		test(Problem21.class, "31626");
+//		test(Problem22.class, "871198282");
+//		test(Problem23.class, "4179871");
+//		test(Problem24.class, "2783915460");
+//		test(Problem25.class, "4782");
+		test(Problem26.class, "983");
 
 
 		test(Problem67.class, "7273");
