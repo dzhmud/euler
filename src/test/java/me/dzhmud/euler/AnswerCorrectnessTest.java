3031ef531251bed661d531458fc43fed4375dea8
dzhmud <zhmud.dmitriy@gmail.com>
2017-11-05 11:41:58 +0200

    Problem 44

--- src/test/java/me/dzhmud/euler/AnswerCorrectnessTest.java
+++ src/test/java/me/dzhmud/euler/AnswerCorrectnessTest.java
@@ -61,10 +61,11 @@ public class AnswerCorrectnessTest {
 //		test(Problem40.class, "210");
 //		test(Problem41.class, "7652413");
 //		test(Problem42.class, "162");
-		test(Problem43.class, "16695334890");
+//		test(Problem43.class, "16695334890");
+		test(Problem44.class, "5482660");
 
 
-//		test(Problem67.class, "7273");
+		test(Problem67.class, "7273");
 	}
 
 	private static void test(Class<? extends EulerSolution> clazz, String answer) {
