b374ff55c98c539bae33f35b95cf1c0446467fbe
dzhmud <zhmud.dmitriy@gmail.com>
2017-11-08 23:32:34 +0200

    problem 56

---
+++ src/main/java/me/dzhmud/euler/pack5/Problem56.java
@@ -0,0 +1,39 @@
+package me.dzhmud.euler.pack5;
+
+import me.dzhmud.euler.EulerSolution;
+import me.dzhmud.euler.util.DigitsUtils;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+/**
+ * Powerful digit sum.
+ * {@see https://projecteuler.net/problem=56}
+ * Find maximum digit sum among numbers of form a^b, where a,b < 100
+ *
+ * @author dzhmud
+ */
+public class Problem56 implements EulerSolution {
+
+	public static void main(String[] args) {
+		long start = System.currentTimeMillis();
+		System.out.println(new Problem56().getAnswer());
+		System.out.println("Solution take " + (System.currentTimeMillis() - start) / 1000 + " sec");
+	}
+
+	@Override
+	public String getAnswer() {
+		return "" + IntStream.range(1,100).flatMap(a ->
+				IntStream.range(1,100).map(b ->
+						getDigitsSum(BigInteger.valueOf(a).pow(b))
+				)
+		).max().orElseThrow(SolutionNotFoundException::new);
+	}
+
+	private static int getDigitsSum(BigInteger bigInteger) {
+		int[] digits = DigitsUtils.getDigits(bigInteger.toString());
+		return Arrays.stream(digits).sum();
+	}
+
+}
